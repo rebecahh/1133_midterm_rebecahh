536a9dd0dd1a85d69b4cab811640fcfb1169e728
Rebecah Huang <rebecah.m.huang@gmail.com>
2022-11-09 14:39:47 -0500

    modified sketch1 changed shape

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # midterm_rebecahh
+
+revisions:
+--scene1--
+* changed ball shape from flower to circle to move away from "spring" connotation
+* movement of the balls showing the "invisible" ball is more important than the shape
--- sketch.js
+++ sketch.js
@@ -1,45 +1,44 @@
+// scene1
 let balls_1 = [];
-let numBalls_1 = 20;
+let numBalls_1 = 50;
 let balls_2 = [];
-let numBalls_2 = 40;
+let numBalls_2 = 80;
 let balls_3 = [];
-let numBalls_3 = 80;
-// let ballsSpeed = [];
-
+let numBalls_3 = 130;
+let bgColor1 = 0;
+let bgColor2 = 255;
 let specialBall;
 
 function setup() {
   noStroke();
-  createCanvas(800, 800);
-  //scene1
+  createCanvas(windowWidth, windowHeight);
+  // scene1
   for (i = 0; i < numBalls_1; i++) {
-    balls_1[i] = new Ball(random(0, width), random(0, height), 10, random(5, 10), random(100),random(100),random(100),50);
+    balls_1[i] = new Ball(random(width), random(height), 10, random(5, 10), random(120), random(120), random(120), 50);
   }
   for (j = 0; j < numBalls_2; j++) {
-    balls_2[j] = new Ball(random(0, width), random(0, height), 15, random(5, 15), random(150),random(150),random(150),50);
+    balls_2[j] = new Ball(random(width), random(height), 15, random(5, 15), random(180), random(180), random(180), 50);
   }
   for (k = 0; k < numBalls_3; k++) {
-    balls_3[k] = new Ball(random(0, width), random(0, height), 20, random(5, 20), random(255),random(255),random(255),50);
+    balls_3[k] = new Ball(random(width), random(height), 20, random(5, 20), random(255), random(255), random(255), 50);
   }
-
 }
 
 function draw() {
-  if (millis()<15000){
+  if (millis() < 15000) {
     scene1();
   }
-  //scene2
 
 }
 
-function scene1(){
+function scene1() {
   // scene1
   if (millis() < 5000) {
     background(0);
     for (let i = 0; i < balls_1.length; i++) {
       balls_1[i].move();
       balls_1[i].display();
-      specialBall = new Ball(width / 2, height / 2, 0, 0, 0, 0, 0,50);
+      specialBall = new Ball(width / 2, height / 2, 0, 0, 0, 0, 0, 100);
       specialBall.display();
     }
   }
@@ -51,7 +50,7 @@ function scene1(){
     for (let j = 0; j < balls_2.length; j++) {
       balls_2[j].move();
       balls_2[j].display();
-      specialBall = new Ball(width / 2, height / 2, 0, 0, 0, 0, 0,50);
+      specialBall = new Ball(width / 2, height / 2, 0, 0, 0, 0, 0, 100);
       specialBall.display();
     }
   }
@@ -63,17 +62,20 @@ function scene1(){
     for (let k = 0; k < balls_3.length; k++) {
       balls_3[k].move();
       balls_3[k].display();
-      specialBall = new Ball(width / 2, height / 2, 0, 0, 0, 0, 0,50);
+      specialBall = new Ball(width / 2, height / 2, 0, 0, 0, 0, 0, 100);
       specialBall.display();
     }
   }
-  if (millis() > 13000) {
-    fill(r,g,b,10)
+  // fade to white
+  if (millis() > 13000 && millis() < 15000) {
+    background(bgColor1);
+    bgColor1+=5;
   }
 }
 
+//scene1
 class Ball {
-  constructor(x, y, xIncr, yIncr, r, g, b,size) {
+  constructor(x, y, xIncr, yIncr, r, g, b, size) {
     this.x = x;
     this.y = y;
     this.xIncr = xIncr;
@@ -85,6 +87,7 @@ class Ball {
   }
   move() {
     this.x += this.xIncr;
+
     if (this.x < 0 || this.x > width) {
       this.xIncr *= -1;
     }
@@ -95,9 +98,6 @@ class Ball {
   }
   display() {
     fill(this.r, this.g, this.b);
-    ellipse(this.x-30, this.y, this.size,this.size - 20);
-    ellipse(this.x+30, this.y, this.size,this.size - 20);
-    ellipse(this.x, this.y+30, this.size-20,this.size);
-    ellipse(this.x, this.y-30, this.size-20,this.size);
+    circle(this.x, this.y, this.size);
   }
 }
